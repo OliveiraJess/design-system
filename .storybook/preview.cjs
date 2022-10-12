24deb62fa875ff084130a898fff7f9bfd22a28bb
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-10-12 20:50:28 -0300

    utilizando Text stories

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,4 +1,4 @@
-import { themes } from '@storybook/theming';
+// import { themes } from '@storybook/theming';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,7 +8,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  docs: {
-    theme: themes.dark
-  },
+  // docs: {
+  //   theme: themes.dark
+  // },
 }
---
+++ src/components/Text.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Text() {
+  return (
+    <>
+      <h1>Text</h1>
+    </>
+  )
+};
