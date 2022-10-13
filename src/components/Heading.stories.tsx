98cf5c2d020241b3b0db0465578b2452956c9559
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-10-13 10:19:41 -0300

    configurando heading stories

---
+++ src/components/Heading.stories.tsx
@@ -0,0 +1,56 @@
+import { Slot } from '@radix-ui/react-slot';
+import { Meta, StoryObj } from '@storybook/react';
+import { Heading, HeadingProps } from "./Heading";
+
+export default {
+  title: 'Components/Heading',
+  component: Heading,
+  args: {
+    children: 'Heading text text text',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<HeadingProps>
+
+
+export const Default: StoryObj<HeadingProps> = {};
+
+export const Small: StoryObj<HeadingProps> = {
+  args: {
+    size: 'sm',
+  },
+};
+
+export const Large: StoryObj<HeadingProps> = {
+  args: {
+    size: 'lg',
+  },
+};
+
+export const CustomComponent: StoryObj<HeadingProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <h1>Heading with h1</h1>
+    ),
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      }
+    },
+    asChild: {
+      table: {
+        disable: true,
+      }
+    },
+  },
+};
