08cc27b768c04be607fe3e4f5f8abd7d02f89145
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-10-14 14:13:04 -0300

    criando page SignIn com o conteudo de App, passando SignIn para App e criando state booleano de login

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,73 @@
+import { FormEvent } from 'react';
+import { Logo } from '../Logo';
+import { Heading } from '../components/Heading';
+import { Text } from '../components/Text';
+import { TextInput } from '../components/TextInput';
+import { Checkbox } from '../components/Checkbox';
+import { Envelope, Lock } from 'phosphor-react';
+import { Button } from '../components/Button';
+import { useState } from 'react';
+
+export function SignIn() {
+  const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+
+  function handleSignIn(event: FormEvent) {
+    event.preventDefault();
+
+    setIsUserSignedIn(true);
+  };
+
+  return (
+    <div className='w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100' >
+
+      <header className='flex flex-col items-center'>
+        <Logo />
+        <Heading size='lg' className='mt-4'>
+          Ignite Lab
+        </Heading>
+        <Text size='lg' className='text-gray-400 mt-1 '>
+          Faça login e comece a usar!
+        </Text>
+      </header>
+
+      <form onSubmit={handleSignIn} className='flex flex-col gap-4 items-stretch w-full max-w-sm mt-10'>
+        {isUserSignedIn && <Text>Login realizado!</Text>}
+        <label htmlFor='email' className='flex flex-col gap-3' >
+          <Text className='font-semibold'>Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input id='email' type='email' placeholder='example@example.com' />
+          </TextInput.Root>
+        </label>
+        <label htmlFor='password' className='flex flex-col gap-3' >
+          <Text className='font-semibold'>Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input id='password' type='password' placeholder='********' />
+          </TextInput.Root>
+        </label>
+        <label htmlFor='remember' className='flex items-center gap-2 '>
+          <Checkbox id='remember' />
+          <Text size='sm' className='text-gray-200 '>Lembrar de mim por 30 dias</Text>
+        </label>
+
+        <Button type='submit' className='mt-4' >Entrar na plataforma</Button>
+      </form>
+
+      <footer className='flex flex-col items-center gap-4 mt-8'>
+        <Text size='sm' asChild>
+          <a href="" className='text-gray-400 underline hover:text-gray-200 '>Esqueceu sua senha?</a>
+        </Text>
+        <Text size='sm' asChild>
+          <a href="" className='text-gray-400 underline hover:text-gray-200 '>Não possui conta? Crie uma agora!</a>
+        </Text>
+      </footer>
+
+    </div>
+  )
+
+};
