7b8ec16a2a1e3b813535b16e2d05ab6d48ed09f2
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-10-14 10:32:53 -0300

    header em app com logo, heading e text

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './styles/global.css';
-import { Logo } from './Logo'
+import { Logo } from './Logo';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+
 
 function App() {
 
   return (
-    <div className='w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100  ' >
-      <Logo />
+    <div className='w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100' >
+
+      <header className='flex flex-col items-center'>
+        <Logo />
+        <Heading size='lg' className='mt-4'>
+          Ignite Lab
+        </Heading>
+        <Text size='lg' className='text-gray-400 mt-1 '>
+          Faça login e comece a usar!
+        </Text>
+      </header>
+
 
     </div>
   )
