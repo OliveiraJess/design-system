d19152d20635de75f18ad4e9cd7f5288a2ebba45
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-10-14 18:22:58 -0300

    testes

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,14 +7,15 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
   ],
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-vite"
   },
   "features": {
-    "storyStoreV7": true
+    "storyStoreV7": true,
+    "interactionsDebugger": true,
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,12 +1,28 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { SignIn } from "./SignIn";
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 export default {
   title: 'Pages/SignIn',
   component: SignIn,
-  args: { },
+  args: {},
   argTypes: {},
 } as Meta
 
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(canvas.getByPlaceholderText('example@example.com'), 'jess.deoliveira97@gmail.com');
+    userEvent.type(canvas.getByPlaceholderText('********'), '12345678');
+
+    userEvent.click(canvas.getByRole('button'));
+
+    await waitFor(() => {
+      return expect(canvas.getByText('Login realizado!')).toBeInTheDocument();
+    });
+
+  }
+};
